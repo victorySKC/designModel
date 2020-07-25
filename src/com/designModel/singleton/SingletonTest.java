b3857bee8882d49355cd4b7a2fa93f3fbaf8c4b0
司坤城 <sikuncheng@163.com>
2020-07-25 16:53:20 +0800

    first commit

---
+++ src/com/designModel/singleton/SingletonTest.java
@@ -0,0 +1,29 @@
+package com.designModel.singleton;
+/*
+ *
+ * @author sikuncheng
+ * @description 普通单例模式
+ * @name
+ * @date 2020/7/25 16:33
+ * @param
+ * @return
+ */
+public class SingletonTest {
+    //私有静态实例，目的是实现延迟加载
+    private static SingletonTest ourInstance = null;
+
+    //静态方法，创建实例
+    public static SingletonTest getInstance() {
+        if (ourInstance == null)
+            ourInstance = new SingletonTest();
+        return ourInstance;
+    }
+    //构造私有方法，防止被实例化
+    private SingletonTest() {
+    }
+
+    //如果该对象被序列号，可以保证序列号前后一致
+    public Object readResolve(){
+        return ourInstance;
+    }
+}
