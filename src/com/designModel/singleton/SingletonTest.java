e67427877a3843b49265e045b8203381910c3e4a
司坤城 <sikuncheng@163.com>
2020-07-27 17:42:25 +0800

    添加单例模式内容，但是没有全部理解

--- src/com/designModel/singleton/SingletonTest.java
+++ src/com/designModel/singleton/SingletonTest.java
@@ -2,7 +2,7 @@ package com.designModel.singleton;
 /*
  *
  * @author sikuncheng
- * @description 普通单例模式
+ * @description 普通单例模式，使用锁
  * @name
  * @date 2020/7/25 16:33
  * @param
@@ -12,15 +12,21 @@ public class SingletonTest {
     //私有静态实例，目的是实现延迟加载
     private static SingletonTest ourInstance = null;
 
-    //静态方法，创建实例
-    public static SingletonTest getInstance() {
-        if (ourInstance == null)
-            ourInstance = new SingletonTest();
-        return ourInstance;
-    }
     //构造私有方法，防止被实例化
     private SingletonTest() {
     }
+
+    //静态方法，创建实例
+    public static SingletonTest getInstance() {
+        if (ourInstance == null){
+            synchronized (ourInstance){
+                if (ourInstance == null)
+                    ourInstance = new SingletonTest();
+            }
+        }
+        return ourInstance;
+    }
+
 
     //如果该对象被序列号，可以保证序列号前后一致
     public Object readResolve(){
